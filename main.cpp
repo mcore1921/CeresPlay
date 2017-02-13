0199431a58150923373e5e8de43cf690cb54c6a0
mcore1921 <mc6213449@gmail.com>
2017-02-12 23:53:17 -0800

    Update data, run once with actScalar held constant

--- main.cpp
+++ main.cpp
@@ -35,14 +35,13 @@ int main(int argc, char** argv) {
     double pct = (double)wdd.m_dayNum / (double)daysVector.size();   
     wdd.m_parameterDim = pct*numParameterDims;
   }
+  std::cout << "Loaded " << daysVector.size() << " data points." << std::endl;
 
   // The variable to solve for with its initial value.
   double initial_actScalar = 1.0;
   double *actScalar = new double[numParameterDims];
   for (int i = 0; i < numParameterDims; i++)
-  {
     actScalar[i] = initial_actScalar;
-  }
   double initial_weightOffset = 0.0;
   double weightOffset[1];
   weightOffset[0] = initial_weightOffset;
@@ -57,19 +56,30 @@ int main(int argc, char** argv) {
     new AutoDiffCostFunction<HBCostFunctor, ceres::DYNAMIC, numParameterDims, 1>(hbcf, daysVector.size());
   problem.AddResidualBlock(cost_function, NULL, actScalar, weightOffset);
 
-//  problem.SetParameterBlockConstant(&weightOffset);
+  problem.SetParameterBlockConstant(actScalar);
+//  problem.SetParameterBlockConstant(weightOffset);
 
   // Run the solver!
   Solver::Options options;
 //  options.linear_solver_type = ceres::DENSE_QR;
-  options.minimizer_progress_to_stdout = true;
+//  options.minimizer_progress_to_stdout = true;
   Solver::Summary summary;
   Solve(options, &problem, &summary);
 
-  std::cout << summary.BriefReport() << "\n";
+  std::cout << "actScalar held constant, weightOffset : " 
+	    << initial_weightOffset
+            << " -> " << weightOffset[0] << std::endl;
+
+
+  for (int i = 0; i < numParameterDims; i++)
+    actScalar[i] = initial_actScalar;
+  weightOffset[0] = initial_weightOffset;
+  problem.SetParameterBlockVariable(actScalar);
+  Solve(options, &problem, &summary);
+
+//  std::cout << summary.BriefReport() << "\n";
 //  std::cout << summary.FullReport() << "\n";
 
-  std::cout << "Loaded " << daysVector.size() << " data points." << std::endl;
   std::cout << "actScalar : " << initial_actScalar;
   for (int i = 0; i < numParameterDims; i++)
   {
